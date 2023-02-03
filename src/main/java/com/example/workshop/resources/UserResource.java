9a81e16c2df3daa6c847ddd4495f60aa0b47560d
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-03 05:54:43 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/workshop/resources/UserResource.java
+++ src/main/java/com/example/workshop/resources/UserResource.java
@@ -1,6 +1,6 @@
-package resources;
+package com.example.workshop.resources;
 
-import entities.User;
+import com.example.workshop.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
