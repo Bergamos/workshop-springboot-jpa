eba2412db721f93696d77d30a06d6578d74c6bc2
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-03 06:30:02 -0300

    Service layer, component registration

---
+++ src/main/java/com/example/workshop/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.workshop.services;
+
+import com.example.workshop.entities.User;
+import com.example.workshop.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+
+    public User findByID(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
