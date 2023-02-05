3e3f11a77b3aa93c2e4526eabb23ebb8a54612d7
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-05 08:18:16 -0300

    Product

---
+++ src/main/java/com/example/workshop/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.workshop.repositories;
+
+import com.example.workshop.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
