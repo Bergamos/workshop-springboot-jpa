f06e689e55816507f198f720e5a597fd3efd6893
Alisson Bergamin <alisson.bergs@gmail.com>
2023-02-06 05:42:37 -0300

    Exception handling - delete

--- src/main/java/com/example/workshop/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/workshop/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.workshop.resources.exceptions;
 
+import com.example.workshop.services.exceptions.DataBaseException;
 import com.example.workshop.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -21,5 +22,12 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database (DataBaseException e, HttpServletRequest request) {
 
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 }
---
+++ src/main/java/com/example/workshop/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.example.workshop.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException (String msg) {
+        super(msg);
+    }
+}
